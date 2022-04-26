8144af9df1ad0760e5126b796d3a9fc258c55b40
IrunaBalandiukh <balandiukh.i@gmail.com>
2022-04-26 10:05:43 +0300

    hw4

---
+++ src/constants/urls.js
@@ -0,0 +1,10 @@
+const baseURl = process.env.REACT_APP_API
+
+const urls = {
+    users: '/users',
+    posts: '/posts'
+}
+
+export default baseURl
+
+export {urls}
---
+++ src/services/axios.service.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+import baseURl from "../constants/urls";
+
+const axiosService = axios.create({baseURL:baseURl});
+
+export {axiosService};
